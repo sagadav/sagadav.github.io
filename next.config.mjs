e950d17519e0a2fe5af6ca3489277233e1bd4bb9
razrab345 <razrab345@gmail.com>
2025-03-15 21:20:20 +0500

    update

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import createMDX from '@next/mdx';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
 };
 
