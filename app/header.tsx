090523173171dd97d7e791cb59b9355c2d74f1b8
razrab345 <razrab345@gmail.com>
2025-03-15 21:27:10 +0500

    update

--- app/header.tsx
+++ app/header.tsx
@@ -16,7 +16,7 @@ export function Header() {
           className="text-zinc-600 dark:text-zinc-500"
           delay={0.5}
         >
-          Web-dev
+          Web-developer
         </TextEffect>
       </div>
     </header>
